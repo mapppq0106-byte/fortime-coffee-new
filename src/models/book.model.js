db3cd43623a1513c186cb036315acdf8eb79a7e4
mapppq0106-byte <mapppq0106@gmail.com>
2026-03-22 12:17:57 +0700

    Delete src directory

--- src/models/book.model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../config/db')
-
-const Book = {
-    getAll: async () => {
-        const [rows] = await db.execute('SELECT * FROM books');
-        return rows
-    }
-}
-module.exports = Book 
--- src/routes/book.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const bookController = require('../controllers/book.controller')
-
-router.get('/', bookController.getAllBooks)
-
-module.exports = router 
